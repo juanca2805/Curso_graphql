b1106e0786cf15e3a10e301caa21896dd7194f4a
juanca0108 <109818872+juanca2805@users.noreply.github.com>
2023-12-20 11:17:58 -0500

    Se hace la creacion del schema del usuario

--- graphql-mysql/src/resolvers/query/user.ts
+++ graphql-mysql/src/resolvers/query/user.ts
@@ -2,6 +2,17 @@ import { IResolvers } from 'graphql-tools';
 
 const resolverQueryUsers: IResolvers = {
     Query: {
+        users() {
+            return [
+                {
+                    id:1,
+                    name: 'anartz mugika ledo',
+                    instructor: 1,
+                    twitter: 'mugan86',
+                    web: ''
+                }
+            ]
+        },
         hello(): string {
             return 'Hello world!!';
         },
