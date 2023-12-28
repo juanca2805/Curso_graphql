33456242e02e33c649e209cca44eed0a132030c0
juanca0108 <109818872+juanca2805@users.noreply.github.com>
2023-12-28 11:55:57 -0500

    Se obtienen ya los datos de la base de datos

--- graphql-mysql/src/resolvers/query/user.ts
+++ graphql-mysql/src/resolvers/query/user.ts
@@ -1,17 +1,27 @@
 import { IResolvers } from 'graphql-tools';
+import { USER_LIST } from '../../constants/db-operations';
 
 const resolverQueryUsers: IResolvers = {
     Query: {
-        users() {
-            return [
-                {
-                    id:1,
-                    name: 'anartz mugika ledo',
-                    instructor: 1,
-                    twitter: 'mugan86',
-                    web: ''
-                }
-            ]
+        users(_, __, {connection}) {
+        const users = new Array(0);
+        var sql = USER_LIST;
+        return new Promise((resolve, reject)=>{
+              connection.query(sql, function (error: any, results: any) {
+        if (error) { reject([]);}
+        // Resultado correcto
+        results.forEach((element: any) => {
+            users.push({
+                id: element.id,
+                name: element.name,
+                instructor: element.instructor,
+                twitter: element.twitter,
+                web: element.web
+            });
+        });
+        resolve(users);
+        });
+        });
         },
         hello(): string {
             return 'Hello world!!';
